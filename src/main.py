a5bb70a98de148da0accf31f3dfded40b3ad64be
BillJWatts <bill.j.watts@gmail.com>
2023-09-16 22:02:29 +0100

    New pages and error handling

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -11,8 +11,8 @@ templates = Jinja2Templates(directory="src/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def home_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+async def home_page():
+    return RedirectResponse(url="/about_me")
 
 
 @app.get("/about_me", response_class=HTMLResponse)
@@ -20,11 +20,26 @@ async def about_me_page(request: Request):
     return templates.TemplateResponse("about_me.html", {"request": request})
 
 
-@app.get("/blog", response_class=HTMLResponse)
+@app.get("/blog/", response_class=HTMLResponse)
 async def blog_page(request: Request):
-    return templates.TemplateResponse("blog.html", {"request": request})
+    return templates.TemplateResponse("blogs/home.html", {"request": request})
+
+
+@app.get("/blog/dev", response_class=HTMLResponse)
+async def blog_page(request: Request):
+    return templates.TemplateResponse("blogs/dev.html", {"request": request})
 
 
 @app.get("/projects", response_class=HTMLResponse)
 async def projects_page(request: Request):
-    return templates.TemplateResponse("projects.html", {"request": request})
+    return templates.TemplateResponse("projects/home.html", {"request": request})
+
+
+@app.get("/projects/discordbots", response_class=HTMLResponse)
+async def discord_bots_page(request: Request):
+    return templates.TemplateResponse("projects/discord_bots.html", {"request": request})
+
+
+@app.exception_handler(404)
+async def four_oh_four(request: Request, _):
+    return templates.TemplateResponse("errors/four_oh_four.html", {"request": request})
