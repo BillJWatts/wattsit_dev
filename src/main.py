06a2f750b05bc8762adc56f987571fe7c3419d53
BillJWatts <bill.j.watts@gmail.com>
2026-07-31 23:06:10 +0000

    Update dependencies and jinja2 API

--- src/main.py
+++ src/main.py
@@ -12,29 +12,29 @@ templates = Jinja2Templates(directory="src/templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def about_me_page(request: Request):
-    return templates.TemplateResponse("about_me.html", {"request": request})
+    return templates.TemplateResponse(request, "about_me.html")
 
 
 @app.get("/blog/", response_class=HTMLResponse)
 async def blog_page(request: Request):
-    return templates.TemplateResponse("blogs/home.html", {"request": request})
+    return templates.TemplateResponse(request, "blogs/home.html")
 
 
 @app.get("/blog/dev", response_class=HTMLResponse)
 async def blog_page(request: Request):
-    return templates.TemplateResponse("blogs/dev.html", {"request": request})
+    return templates.TemplateResponse(request, "blogs/dev.html")
 
 
 @app.get("/projects", response_class=HTMLResponse)
 async def projects_page(request: Request):
-    return templates.TemplateResponse("projects/home.html", {"request": request})
+    return templates.TemplateResponse(request, "projects/home.html")
 
 
 @app.get("/projects/discordbots", response_class=HTMLResponse)
 async def discord_bots_page(request: Request):
-    return templates.TemplateResponse("projects/discord_bots.html", {"request": request})
+    return templates.TemplateResponse(request, "projects/discord_bots.html")
 
 
 @app.exception_handler(404)
 async def four_oh_four(request: Request, _):
-    return templates.TemplateResponse("errors/four_oh_four.html", {"request": request})
+    return templates.TemplateResponse(request, "errors/four_oh_four.html")
